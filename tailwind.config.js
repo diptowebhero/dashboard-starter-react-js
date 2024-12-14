e538ea07d9f03c54bc25e2af18d1b259d014fed2
diptowebhero <diptodas1672@gmail.com>
2024-12-14 18:37:58 +0600

    feat(BaseUI): add buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
-const colors = require("tailwindcss/colors");
+import colors from "tailwindcss/colors";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
